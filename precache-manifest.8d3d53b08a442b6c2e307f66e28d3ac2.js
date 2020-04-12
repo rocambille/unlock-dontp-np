e8844a2004a1c84452920e9b8985591b09f1b549
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2020-04-12 16:18:38 +0200

    Updates

---
+++ precache-manifest.8d3d53b08a442b6c2e307f66e28d3ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ee7ae9e7d3f92a7b36fb5bac9ae719",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f92d54a51bfd7422a5ae",
+    "url": "/static/css/main.af7c3565.chunk.css"
+  },
+  {
+    "revision": "9f4e0d8f37839ab248f7",
+    "url": "/static/js/2.4a45fb4d.chunk.js"
+  },
+  {
+    "revision": "34588f639aff350122d4f60131dc42c4",
+    "url": "/static/js/2.4a45fb4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92d54a51bfd7422a5ae",
+    "url": "/static/js/main.7f08d544.chunk.js"
+  },
+  {
+    "revision": "23b3b896c351f054c4cd",
+    "url": "/static/js/runtime-main.ce377dda.js"
+  }
+]);
